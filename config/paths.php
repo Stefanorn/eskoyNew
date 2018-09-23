932fba157282efef8ded13fd836c12653906a8f2
Stefan Hrafnsson <stefanorn92@gmail.com>
2018-09-23 23:27:57 +0000

    Setti inn content frá pabba

--- config/paths.php
+++ config/paths.php
@@ -1,10 +1,10 @@
 <?php 
 
-//define('URL', 'https://www.eskoy.no/');
-define('URL', 'http://localhost:8888/eskoyNew/');
+define('URL', 'https://www.eskoy.no/');
+//define('URL', 'http://localhost:8888/eskoyNew/');
 define('EmailLogin', '');
 define('EmailPasword','');
 define('EmailPort', '465');
 define('EmailHost', 'smtp.gmail.com');
-define('EmailSentFrom', 'testemailsender2000@gmail.com');
-define('EmailSentTo', 'stefanorn92@gmail.com');
+define('EmailSentFrom', '');
+define('EmailSentTo', '');
